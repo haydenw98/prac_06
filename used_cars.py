7451d485a4c4a1a91aa20e62955a53f3c83eb807
Hayden Watts <hayden.watts98@yahoo.com>
2020-10-27 14:10:08 +1000

    added limo and truck to used_cars testing, added __str__ to car, all modifications

--- used_cars.py
+++ used_cars.py
@@ -1,12 +1,8 @@
-"""CP1404/CP5632 Practical - Client code to use the Car class."""
-# Note that the import has a folder (module) in it.
-
-from prac_06.car import Car
+from car import Car
 
 
 def main():
-    """Demo test code to show how to use car class."""
-    my_car = Car(180)
+    my_car = Car("My car", 180)
     my_car.drive(30)
     print("fuel =", my_car.fuel)
     print("odo =", my_car.odometer)
@@ -14,6 +10,20 @@ def main():
 
     print("Car {}, {}".format(my_car.fuel, my_car.odometer))
     print("Car {self.fuel}, {self.odometer}".format(self=my_car))
+
+    limo = Car("Limo", 100)
+    limo.add_fuel(20)
+    print(limo.fuel)
+    limo.drive(115)
+    print(limo.odometer)
+    print(limo)
+    print("Car {self.fuel}, {self.odometer}".format(self=limo))
+
+    truck = Car("truck", 300)
+    truck.add_fuel(50)
+    print(truck.fuel)
+    truck.drive(400)
+    print(truck)
 
 
 main()
