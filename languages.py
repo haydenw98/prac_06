33225e52ac304955f68e435543870a8fbe54c9be
Hayden Watts <hayden.watts98@yahoo.com>
2020-10-27 13:55:42 +1000

    programming languages class and program finished

--- languages.py
+++ languages.py
@@ -1,14 +1,17 @@
 from Programming_language import ProgrammingLanguages
 
+ruby = ProgrammingLanguages("Ruby", "Dynamic", True, 1995)
+python = ProgrammingLanguages("Python", "Dynamic", True, 1991)
+visual_basic = ProgrammingLanguages("Visual Basic", "Static", False, 1991)
+java = ProgrammingLanguages("Java", "Static", True, 1995)
+
 def main():
-    ruby = ProgrammingLanguages("Ruby", "Dynamic", True, 1995)
-    python = ProgrammingLanguages("Python", "Dynamic", True, 1991)
-    visual_basic = ProgrammingLanguages("Visual Basic", "Static", False, 1991)
-    java = ProgrammingLanguages("Java", "Static", True, 1995)
     languages = [ruby, python, visual_basic, java]
     print("The dynamically typed languages are:")
     for language in languages:
         if language.check_dynamic():
             print(language.name)
+    for language in languages:
+        print(language)
 
 main()
